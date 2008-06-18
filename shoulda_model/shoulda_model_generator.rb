78045f6fe32d07a9dfa0816ff66e5e003f6e14f5
Josh Nichols <josh@technicalpickles.com>
2008-06-18 06:09:58 -0400

    Initial shoulda_model.

---
+++ shoulda_model/shoulda_model_generator.rb
@@ -0,0 +1,53 @@
+class ShouldaModelGenerator < Rails::Generator::NamedBase
+  default_options :skip_timestamps => false, :skip_migration => false, :skip_factory => false
+
+  def manifest
+    record do |m|
+      # Check for class naming collisions.
+      m.class_collisions class_path, class_name, "#{class_name}Test"
+
+      # Model, test, and fixture directories.
+      m.directory File.join('app/models', class_path)
+      m.directory File.join('test/unit', class_path)
+      m.directory File.join('test/factories', class_path)
+
+      # Model class, unit test, and fixtures.
+      m.template 'model.rb',      File.join('app/models', class_path, "#{file_name}.rb")
+      m.template 'unit_test.rb',  File.join('test/unit', class_path, "#{file_name}_test.rb")
+
+      unless options[:skip_factory] 
+       	m.template 'factory.rb',  File.join('test/factories', class_path, "#{file_name}.rb")
+      end
+
+      unless options[:skip_migration]
+        m.migration_template 'migration.rb', 'db/migrate', :assigns => {
+          :migration_name => "Create#{class_name.pluralize.gsub(/::/, '')}"
+        }, :migration_file_name => "create_#{file_path.gsub(/\//, '_').pluralize}"
+      end
+    end
+  end
+
+  def factory_line(attribute)
+    unless attribute.name == 'name'
+      "#{file_name}.#{attribute.name} '#{attribute.default}'"
+    else
+      "#{file_name}.add_attribute '#{attribute.name}', '#{attribute.default}'"
+    end
+  end
+
+  protected
+    def banner
+      "Usage: #{$0} #{spec.name} ModelName [field:type, field:type]"
+    end
+
+    def add_options!(opt)
+      opt.separator ''
+      opt.separator 'Options:'
+      opt.on("--skip-timestamps",
+             "Don't add timestamps to the migration file for this model") { |v| options[:skip_timestamps] = v }
+      opt.on("--skip-migration", 
+             "Don't generate a migration file for this model") { |v| options[:skip_migration] = v }
+      opt.on("--skip-fixture",
+             "Don't generation a fixture file for this model") { |v| options[:skip_fixture] = v}
+    end
+end
