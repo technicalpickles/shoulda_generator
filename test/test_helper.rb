c9009c6e12d89ea6b45656797c462db91f0a8b59
Josh Nichols <josh@technicalpickles.com>
2008-10-15 21:54:09 -0400

    Stole a little bit from railties.

---
+++ test/test_helper.rb
@@ -0,0 +1,13 @@
+require 'rubygems'
+gem 'activerecord'
+require 'active_record'
+gem 'actionpack'
+require 'action_controller'
+require 'action_view'
+gem 'rails'
+
+gem 'thoughtbot-shoulda'
+require 'shoulda'
+
+
+require File.join(File.dirname(__FILE__), 'stolen_from_railties')
